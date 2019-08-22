5bbe7d09e784cb8cf3479c2a8d36d69003fed08f
潘立祥 <panlishiang@panlixiangde-MacBook-Pro.local>
2019-08-22 21:08:42 +0800

    GC

---
+++ GCD Practice/GCD Practice/ApiAccess.swift
@@ -0,0 +1,188 @@
+//
+//  ApiAccess.swift
+//  GCD Practice
+//
+//  Created by 潘立祥 on 2019/8/22.
+//  Copyright © 2019 PanLiHsiang. All rights reserved.
+//
+
+import Foundation
+
+import UIKit
+
+protocol DetailInfoProviderDelegate: AnyObject {
+    
+    func detailInfoProvider(didGet result: [DetailInfo])
+    
+}
+
+class DetailInfoProvider {
+    
+    weak var detailInfoDelegate: DetailInfoProviderDelegate?
+    
+    static let shared = DetailInfoProvider()
+    
+    let downloadGroup: DispatchGroup = DispatchGroup()
+    
+    var dataProvider: [DetailInfo] = []
+    {
+
+        didSet {
+            detailInfoDelegate?.detailInfoProvider(didGet: dataProvider)
+        }
+    }
+    
+    func fetchDetailInfo(for offset: TaipeiUrl.Offset) {
+        
+        var completeUrl: URLComponents
+        
+        switch offset {
+            
+        case .fistOffset: completeUrl = TaipeiUrl.Offset.fistOffset.taipeiAPI
+            
+        case .secondOffset: completeUrl = TaipeiUrl.Offset.secondOffset.taipeiAPI
+            
+        case .thirdOffset: completeUrl = TaipeiUrl.Offset.thirdOffset.taipeiAPI
+        }
+        
+        guard let url = completeUrl.url else { return }
+        
+//        downloadGroup.enter()
+        
+        let task = URLSession.shared.dataTask(with: url) {(data, response, error) in
+            
+            if error != nil {
+                
+                print ("dataTask error")
+                
+                return
+            }
+            
+            guard let httpResponse = response as? HTTPURLResponse else { return }
+            
+            let statusCode = httpResponse.statusCode
+            
+            if statusCode >= 200 && statusCode < 300 {
+            
+            guard let data = data, let utf8Text = String(data: data, encoding: .utf8) else { return }
+                
+//                print(utf8Text)
+            
+            do {
+                let decoder = JSONDecoder()
+                
+                let resultInfo = try decoder.decode(TaipeiResult.self, from: data)
+                
+                let detailInfo = resultInfo.result
+                
+                let detailInformation = detailInfo.results
+                
+                self.dataProvider.append(contentsOf: detailInformation)
+                
+                print("API Done")
+            
+            } catch {
+            
+                print ("Decode error!")
+                }
+            }
+            
+//            self.downloadGroup.leave()
+            
+            if statusCode >= 400 && statusCode < 500 {
+                
+                guard let error = error else { return}
+                
+                print(error)
+            }
+        }
+        task.resume()
+        
+//        downloadGroup.notify(queue: .main) { [weak self] in
+//
+//            self?.detailInfoDelegate?.detailInfoProvider(didGet: self?.dataProvider ?? [])
+//
+//        }
+     }
+}
+
+class TaipeiUrl{
+    
+    enum Offset: String {
+        
+        case fistOffset = "0"
+        
+        case secondOffset = "10"
+        
+        case thirdOffset = "20"
+    
+    var taipeiAPI: URLComponents {
+        
+        var taipeiAPI = URLComponents()
+        
+        taipeiAPI.scheme = "https"
+        
+        taipeiAPI.host = "data.taipei"
+        
+        taipeiAPI.path = "/opendata/datalist/apiAccess"
+        
+        taipeiAPI.queryItems = [
+            
+            URLQueryItem(name: "scope", value: "resourceAquire"),
+            
+            URLQueryItem(name: "rid", value: "5012e8ba-5ace-4821-8482-ee07c147fd0a"),
+            
+            URLQueryItem(name: "limit", value: "1"),
+            
+            URLQueryItem(name: "offset", value: rawValue)
+        
+        ]
+        
+            return taipeiAPI
+        }
+    }
+}
+
+struct TaipeiResult: Codable {
+    
+    let result: ResultInfo
+}
+
+struct ResultInfo: Codable {
+    
+    let limit: Int
+    
+    let offset: Int
+    
+    let count: Int
+    
+    let sort: String
+    
+    var results: [DetailInfo]
+    
+}
+
+struct DetailInfo: Codable {
+    
+    let functions: String
+    
+    let area: String
+    
+    let no: String
+    
+    let direction: String
+    
+    let speedLimit: String
+    
+    let location: String
+    
+    let Id: Int
+    
+    let road: String
+    
+    enum CodingKeys: String, CodingKey {
+        case Id = "_id"
+        case speedLimit = "speed_limit"
+        case functions, area, no, direction, location, road
+    }
+}
--- GCD Practice/GCD Practice/ViewController.swift
+++ GCD Practice/GCD Practice/ViewController.swift
@@ -8,13 +8,124 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, DetailInfoProviderDelegate {
+   
+    @IBOutlet weak var firstRoadLabel: UILabel!
+    
+    @IBOutlet weak var firstLimitLabel: UILabel!
+    
+    @IBOutlet weak var secondRoadLabel: UILabel!
+    
+    @IBOutlet weak var secondLimitLabel: UILabel!
+    
+    @IBOutlet weak var thirdRoadLabel: UILabel!
+    
+    @IBOutlet weak var thirdLimitLabel: UILabel!
+    
+    var detaillInfo: [DetailInfo] = []
+    
+    let downloadGroup: DispatchGroup = DispatchGroup()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        DetailInfoProvider.shared.detailInfoDelegate = self
+        
     }
+    
+    @IBAction func groupAPI(_ sender: Any) {
+        
+        DetailInfoProvider.shared.fetchDetailInfo(for: .fistOffset)
+        
+        DetailInfoProvider.shared.fetchDetailInfo(for: .secondOffset)
+        
+        DetailInfoProvider.shared.fetchDetailInfo(for: .thirdOffset)
+    }
+    
+    let semaphore = DispatchSemaphore(value: 1)
+    
+    let queue = DispatchQueue.global()
+    
+    @IBAction func semaphoreAPI(_ sender: Any) {
+        
+        queue.async {[weak self] in
+            if self?.semaphore.wait(timeout: .distantFuture) == .success {
+                
+                DetailInfoProvider.shared.fetchDetailInfo(for: .fistOffset)
+                
+                self?.semaphore.signal()
+            }
+        }
+        
+        queue.async {[weak self] in
+            if self?.semaphore.wait(timeout: .distantFuture) == .success {
+                
+                DetailInfoProvider.shared.fetchDetailInfo(for: .secondOffset)
 
-
+                self?.semaphore.signal()
+            }
+        }
+        
+        queue.async {[weak self] in
+            if self?.semaphore.wait(timeout: .distantFuture) == .success {
+                
+                DetailInfoProvider.shared.fetchDetailInfo(for: .thirdOffset)
+                
+                self?.semaphore.signal()
+            }
+        }
+    }
+    
+    
+    func detailInfoProvider(didGet result: [DetailInfo]) {
+        detaillInfo = result
+        
+        queue.async {[weak self] in
+            if self?.semaphore.wait(timeout: .distantFuture) == .success {
+                
+                DispatchQueue.main.async {
+                    self?.firstRoadLabel.text = self?.detaillInfo[0].road
+                    
+                    self?.firstLimitLabel.text = self?.detaillInfo[0].speedLimit
+                }
+                
+                sleep(1)
+                
+                self?.semaphore.signal()
+            }
+        }
+        
+        queue.async {[weak self] in
+            if self?.semaphore.wait(timeout: .distantFuture) == .success {
+                
+                DispatchQueue.main.async {
+                    self?.secondRoadLabel.text = self?.detaillInfo[1].road
+                    
+                    self?.secondLimitLabel.text = self?.detaillInfo[1].speedLimit
+                }
+                
+                sleep(1)
+                
+                self?.semaphore.signal()
+            }
+        }
+        
+        queue.async {[weak self] in
+            if self?.semaphore.wait(timeout: .distantFuture) == .success {
+                
+                DispatchQueue.main.async {
+                    
+                    self?.thirdRoadLabel.text = self?.detaillInfo[2].road
+                    
+                    self?.thirdLimitLabel.text = self?.detaillInfo[2].speedLimit
+                }
+                
+                sleep(1)
+                
+                self?.semaphore.signal()
+            }
+        }
+        
+    }
 }
 
