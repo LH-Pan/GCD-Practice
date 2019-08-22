cfd51106e3f8e1d822d3597fca7e86b8ad7edf9a
潘立祥 <panlishiang@panlixiangde-MacBook-Pro.local>
2019-08-22 12:28:51 +0800

    create GCD Practice project

---
+++ SelectionViewAssignment/SelectionViewAssignment/SelectionViewProtocol.swift
@@ -0,0 +1,9 @@
+//
+//  SelectionViewProtocol.swift
+//  SelectionViewAssignment
+//
+//  Created by 潘立祥 on 2019/8/19.
+//  Copyright © 2019 PanLiHsiang. All rights reserved.
+//
+
+import Foundation
